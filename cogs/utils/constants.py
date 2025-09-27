a1def74f6f1fd000273f03a567b16d1c749df26e
Hans 展豪 <hansvalerie19@gmail.com>
2025-09-27 22:30:04 +0700

    Create constants.py
    Define main file path used by cogs to access local files

---
+++ cogs/utils/constants.py
@@ -0,0 +1,36 @@
+import os
+
+COG_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # points to ...\AniAvatar\cogs
+ROOT_PATH = os.path.dirname(COG_PATH)                                   # points to ...\AniAvatar
+
+FONT_DIR = os.path.join(ROOT_PATH, "assets", "fonts")
+EMOJI_PATH = os.path.join(ROOT_PATH, "assets", "emojis", "RANK ICONS")
+BG_PATH = os.path.join(ROOT_PATH, "assets", "backgrounds")
+
+FONTS = {
+    "bold": os.path.join(FONT_DIR, "gg sans Bold.ttf"),
+    "medium": os.path.join(FONT_DIR, "gg sans Medium.ttf"),
+    "regular": os.path.join(FONT_DIR, "gg sans Regular.ttf"),
+    "semibold": os.path.join(FONT_DIR, "gg sans Semibold.ttf"),
+}
+
+TITLE_EMOJI_FILES = {
+    "Novice": os.path.join(EMOJI_PATH, "NOVICE.png"),
+    "Warrior": os.path.join(EMOJI_PATH, "WARRIOR.png"),
+    "Elite": os.path.join(EMOJI_PATH, "ELITE.png"),
+    "Champion": os.path.join(EMOJI_PATH, "CHAMPION.png"),
+    "Hero": os.path.join(EMOJI_PATH, "HERO.png"),
+    "Legend": os.path.join(EMOJI_PATH, "LEGEND.png"),
+    "Mythic": os.path.join(EMOJI_PATH, "MYTHIC.png"),
+    "Ascendant": os.path.join(EMOJI_PATH, "ASCENDANT.png"),
+    "Immortal": os.path.join(EMOJI_PATH, "IMMORTAL.png"),
+    "Celestial": os.path.join(EMOJI_PATH, "CELESTIAL.png"),
+    "Transcendent": os.path.join(EMOJI_PATH, "TRANSCENDENT.png"),
+    "Aetherborn": os.path.join(EMOJI_PATH, "AETHERBORN.png"),
+    "Cosmic": os.path.join(EMOJI_PATH, "COSMIC.png"),
+    "Divine": os.path.join(EMOJI_PATH, "DIVINE.png"),
+    "Eternal": os.path.join(EMOJI_PATH, "ETERNAL.png"),
+    "Enlightened": os.path.join(EMOJI_PATH, "ENLIGHTENED.png"),
+}
+
+
